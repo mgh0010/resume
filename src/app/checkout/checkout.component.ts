28cf0b2ecd122f11aa17c0612ed57291b301074d
Michael <mgh0010@auburn.edu>
2018-10-03 02:34:05 -0500

    can get to all screens easily now

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import { AnimationService } from '../services/animation.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements AfterViewInit {
+  time = 0;
 
-  constructor() { }
+  constructor(public as: AnimationService) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.as.animateIn('#man-for-job-txt', 'flipInX');
+    }, this.time);
+    this.time += 250;
+    setTimeout(() => {
+      this.as.animateIn('#me-img', 'fadeIn');
+    }, this.time);
+    this.loadInBtns();
   }
 
+  private loadInBtns() {
+    setTimeout(() => {
+      this.as.animateIn('#work-history-btn', 'fadeInUp');
+    }, this.time);
+    setTimeout(() => {
+      this.as.animateIn('#projects-btn', 'fadeInUp');
+    }, this.time);
+  }
 }
