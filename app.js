000dac090abfedc47308aac3e5b5148c375d7249
Harrison, Michael <michael.harrison.contractor@bbva.com>
2019-09-05 11:11:18 -0500

    updated compression and git ignore

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const shrinkRay = require('shrink-ray-current');
 const http = require('http');
 const path = require('path');
 const app = express();
+
+app.use(shrinkRay());
+
 app.use(express.static(path.join(__dirname, 'dist', 'resume')));
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist', 'resume', 'index.html'));
