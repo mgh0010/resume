64d15e885fcc2d4e97e942ba5c2c5f8f253852dd
Harrison, Michael <michael.harrison.contractor@bbva.com>
2020-12-22 13:32:40 -0600

    updated a tiny bit

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { AnimationService } from '../services/animation.service';
+import { jobs } from './jobs';
 
 declare var $: any;
 
@@ -10,74 +11,7 @@ declare var $: any;
 })
 export class JobsComponent implements OnInit, AfterViewInit {
   time = 0;
-  jobs = {
-    'Crossvale': {
-      'title': 'Associate Developer',
-      'company': 'Crossvale',
-      'location': 'Office: Birmingham, AL | HQ: Dallas, TX',
-      'time': 'October 2018 - Present',
-      'description': `Engineer, develop, and design BBVA Compass Bank front-end software currently in angularJS (soon to be Polymer). 
-                      This application allows in branch employees to perform bank related matters on the web.`,
-      'icon': 'code'
-    },
-    'together': {
-      'title': 'Lead Web Developer',
-      'company': 'Together',
-      'location': 'Remote',
-      'time': 'October 2019 - Jan 2020',
-      'description': `Add to and maintain a Web Administration Portal for Churches to manage their congregation. 
-                      Built with Vue 3 and Vuex. Push notifications handled with firebase`,
-      'icon': 'code'
-    },
-    'selex': {
-      'title': 'Software Engineer',
-      'company': 'Selex Galileo Inc',
-      'location': 'Huntsville, AL',
-      'time': 'August 2018 - October 2018',
-      'description': 'Build and maintain C++ Electronic Warfare Avionic Systems with Rhapsody and Visual Studio.',
-      'icon': 'airplanemode_active'
-    },
-    'saturn': {
-      'title': 'Angular Software Developer',
-      'company': 'Saturn Ergonomics',
-      'location': 'Auburn, AL',
-      'time': 'May 2018 - Aug 2018',
-      'description': `Research, Design, and development a web-based suite of calculators for the ergonomic industry.
-        The web application consists of an Angular 6 front-end with flask server back-end in python with MySQL
-        database.`,
-      'icon': 'code'
-    },
-    'adtran1': {
-      'title': 'User Experience Co-op',
-      'company': 'ADTRAN',
-      'location': 'Huntsville, AL',
-      'time': 'May 2016 - Aug 2016',
-      'description': `Design User interfaces per User Experience guidelines for several widgets in
-        ADTRAN's MOSAIC framework. Design these widgets by means of heavy research, user testing,
-        gathering feedback, drawing mockups, presenting mockups, and refining designs to best meet user needs.`,
-      'icon': 'widgets'
-    },
-    'adtran2': {
-      'title': 'System Software Team Co-op',
-      'company': 'ADTRAN',
-      'location': 'Huntsville, AL',
-      'time': 'Aug 2015 - Dec 2015',
-      'description': `Optimize current data structure algorithms in C++ and C using STL containers.
-        Construct more efficient data structure for OS timer handling and implement into the existing
-        kernel code. Write many tests for own code with gtest framework.`,
-      'icon': 'sd_card'
-    },
-    'adtran3': {
-      'title': 'Systems Design and Verification Test Co-op',
-      'company': 'ADTRAN',
-      'location': 'Huntsville, AL',
-      'time': 'Jan 2015 - May 2015',
-      'description': `Work closely with a Triple Play Testbed. Upgrade the network, configure new services
-        and write python scripts and python app to speed up redundant interaction with testbed and other
-        telnet accessible lab equipment.`,
-      'icon': 'build'
-    }
-  };
+  jobs = jobs;
 
   constructor(public as: AnimationService) { }
 
---
+++ src/app/jobs/jobs.ts
@@ -0,0 +1,86 @@
+export const jobs = {
+  'Crossvale2': {
+    'title': 'Senior Tech Lead',
+    'company': 'Crossvale',
+    'location': 'Remote',
+    'time': 'April 2020 - Present',
+    'description': `
+Act as the technical face of our agile team, communicating effectively with teams across the bank to investigate the current microservices available and plan to implement custom solutions using them.
+
+Manage, encourage, and lead a team of developers to be the best they can be.
+
+Build lit-element applications implemented with a micro-frontend architecture
+
+Emphasize the importance of web component reusability and best practices
+
+Continue duties listed below before promotion.`,
+    'icon': 'code'
+  },
+  'Crossvale1': {
+    'title': 'Associate Developer',
+    'company': 'Crossvale',
+    'location': 'Office: Birmingham, AL | HQ: Dallas, TX',
+    'time': 'October 2018 - April 2020',
+    'description': `
+Engineer, develop, and design BBVA Compass Bank front-end software currently in angularJS (soon to be Polymer).
+This application allows in branch employees to perform bank related matters on the web.`,
+    'icon': 'code'
+  },
+  'together': {
+    'title': 'Lead Web Developer',
+    'company': 'Together',
+    'location': 'Remote',
+    'time': 'October 2019 - Jan 2020',
+    'description': `Add to and maintain a Web Administration Portal for Churches to manage their congregation. 
+                      Built with Vue 3 and Vuex. Push notifications handled with firebase`,
+    'icon': 'code'
+  },
+  'selex': {
+    'title': 'Software Engineer',
+    'company': 'Selex Galileo Inc',
+    'location': 'Huntsville, AL',
+    'time': 'August 2018 - October 2018',
+    'description': 'Build and maintain C++ Electronic Warfare Avionic Systems with Rhapsody and Visual Studio.',
+    'icon': 'airplanemode_active'
+  },
+  'saturn': {
+    'title': 'Angular Software Developer',
+    'company': 'Saturn Ergonomics',
+    'location': 'Auburn, AL',
+    'time': 'May 2018 - Aug 2018',
+    'description': `Research, Design, and development a web-based suite of calculators for the ergonomic industry.
+        The web application consists of an Angular 6 front-end with flask server back-end in python with MySQL
+        database.`,
+    'icon': 'code'
+  },
+  'adtran1': {
+    'title': 'User Experience Co-op',
+    'company': 'ADTRAN',
+    'location': 'Huntsville, AL',
+    'time': 'May 2016 - Aug 2016',
+    'description': `Design User interfaces per User Experience guidelines for several widgets in
+        ADTRAN's MOSAIC framework. Design these widgets by means of heavy research, user testing,
+        gathering feedback, drawing mockups, presenting mockups, and refining designs to best meet user needs.`,
+    'icon': 'widgets'
+  },
+  'adtran2': {
+    'title': 'System Software Team Co-op',
+    'company': 'ADTRAN',
+    'location': 'Huntsville, AL',
+    'time': 'Aug 2015 - Dec 2015',
+    'description': `Optimize current data structure algorithms in C++ and C using STL containers.
+        Construct more efficient data structure for OS timer handling and implement into the existing
+        kernel code. Write many tests for own code with gtest framework.`,
+    'icon': 'sd_card'
+  },
+  'adtran3': {
+    'title': 'Systems Design and Verification Test Co-op',
+    'company': 'ADTRAN',
+    'location': 'Huntsville, AL',
+    'time': 'Jan 2015 - May 2015',
+    'description': `Work closely with a Triple Play Testbed. Upgrade the network, configure new services
+        and write python scripts and python app to speed up redundant interaction with testbed and other
+        telnet accessible lab equipment.`,
+    'icon': 'build'
+  }
+}
