8e177007a32c679035da1380f3e3f8b3595df505
Harrison, Michael <michael.harrison.contractor@bbva.com>
2020-12-22 14:36:34 -0600

    updated info

--- src/app/jobs/jobs.ts
+++ src/app/jobs/jobs.ts
@@ -2,7 +2,7 @@ export const jobs = {
   'Crossvale2': {
     'title': 'Senior Tech Lead',
     'company': 'Crossvale',
-    'location': 'Remote',
+    'location': 'Remote | Full-time',
     'time': 'April 2020 - Present',
     'description': `
 Act as the technical face of our agile team, communicating effectively with teams across the bank to investigate the current microservices available and plan to implement custom solutions using them.
@@ -19,68 +19,137 @@ Continue duties listed below before promotion.`,
   'Crossvale1': {
     'title': 'Associate Developer',
     'company': 'Crossvale',
-    'location': 'Office: Birmingham, AL | HQ: Dallas, TX',
+    'location': 'Birmingham, Al | Full-time',
     'time': 'October 2018 - April 2020',
     'description': `
-Engineer, develop, and design BBVA Compass Bank front-end software currently in angularJS (soon to be Polymer).
-This application allows in branch employees to perform bank related matters on the web.`,
+Engineer and develop lasting web software for BBVA Bank using JS, HTML, CSS, Polymer, lit-element, angularJs, and Bootstrap.
+
+Refactor 1000’s of lines of legacy code implementing best coding standards.
+
+Constantly consume microservice API’s from various different teams.
+
+Use Git for all source control needs. Constantly commit and handle merge conflicts.
+
+Help design features by providing the team with technical limitations.
+
+Unit test code and participate in code reviews.
+
+Interview potential candidates for open positions.
+
+Explore new ideas and innovations with existing technologies in use to aid the company.
+
+Agile(SAFe) methodology following two week sprints, standups, and backlog ceremonies.
+
+Use Jira and its related suite of Atlassian products.
+`,
+    'icon': 'code'
+  },
+  'copycodes': {
+    'title': 'Co-Owner and Lead Developer',
+    'company': 'CopyCodes | https://copycodes.io',
+    'location': 'Remote | Part-time',
+    'time': 'September 2020 - Present',
+    'description': `
+Fully develop everything for CopyCodes.
+
+Build the front end Progressive Web App with Ionic and Angular 11 with typescript.
+
+Build an influencer management App for easier management of the platform.
+
+Build the backend with Firebase's Hosting, Firestore, Storage, Functions, Authentication, Stripe to accept payments and payout influencers, Twilio to send texts, and multiple Node js Google App Engine projects to handle backend tasks
+
+Customize the domain and setup an entirely separate QA region with it's own test data and isolated front end
+    `,
+    'icon': 'code'
+  },
+  'kudos': {
+    'title': 'Lead Software Engineer',
+    'company': 'KUDOS',
+    'location': 'Remote | Contract / Part-time',
+    'time': 'April 2020 - October 2020',
+    'description': `
+Build a fully functioning backend with Google Cloud Platform and nosql Firebase.
+
+Implement Firebase's Hosting, Firestore, Storage, Functions, and phone number Authentication
+
+Build several Google App Engine node servers, both flex and standard, capable of merging videos together, storing videos in firebase storage, interacting the database, and sending texts.
+
+Implement deep/dynamic linking in the app for text invitations.
+
+Review and approve merge requests of the front end developer coding in React Native.
+`,
     'icon': 'code'
   },
   'together': {
-    'title': 'Lead Web Developer',
+    'title': 'Lead Software Engineer',
     'company': 'Together',
-    'location': 'Remote',
+    'location': 'Remote | Contract / Part-time',
     'time': 'October 2019 - Jan 2020',
-    'description': `Add to and maintain a Web Administration Portal for Churches to manage their congregation. 
-                      Built with Vue 3 and Vuex. Push notifications handled with firebase`,
+    'description': `
+Add to and maintain a Web Administration Portal for Churches to manage their congregation.
+
+Develop features such as people management, giving/tithes, teams, and events.
+
+Use Vue 3 for development, replace existing state management with Vuex, and use Firebase cloud for push notifications.
+
+Manage and orchestrate a small team of devs in day to day work with continual deployments.
+`,
     'icon': 'code'
   },
   'selex': {
     'title': 'Software Engineer',
     'company': 'Selex Galileo Inc',
-    'location': 'Huntsville, AL',
+    'location': 'Huntsville, AL | Full-time',
     'time': 'August 2018 - October 2018',
-    'description': 'Build and maintain C++ Electronic Warfare Avionic Systems with Rhapsody and Visual Studio.',
+    'description': `
+Build and maintain C++ Electronic Warfare Avionic Systems with Rhapsody and Visual Studio.
+`,
     'icon': 'airplanemode_active'
   },
   'saturn': {
     'title': 'Angular Software Developer',
     'company': 'Saturn Ergonomics',
-    'location': 'Auburn, AL',
+    'location': 'Auburn, AL | Part-time',
     'time': 'May 2018 - Aug 2018',
-    'description': `Research, Design, and development a web-based suite of calculators for the ergonomic industry.
-        The web application consists of an Angular 6 front-end with flask server back-end in python with MySQL
-        database.`,
+    'description': `
+Research, Design, and development a web-based suite of calculators for the ergonomic industry.
+
+The web application consists of an Angular 6 front-end with flask server back-end in python with noSQL database.`,
     'icon': 'code'
   },
   'adtran1': {
     'title': 'User Experience Co-op',
     'company': 'ADTRAN',
-    'location': 'Huntsville, AL',
+    'location': 'Huntsville, AL | Full-time',
     'time': 'May 2016 - Aug 2016',
-    'description': `Design User interfaces per User Experience guidelines for several widgets in
-        ADTRAN's MOSAIC framework. Design these widgets by means of heavy research, user testing,
-        gathering feedback, drawing mockups, presenting mockups, and refining designs to best meet user needs.`,
+    'description': `
+Design User interfaces per User Experience guidelines for several widgets in ADTRAN's MOSAIC framework.
+
+Design widgets by means of heavy research, user testing, gathering feedback, drawing mockups, presenting mockups, and refining designs to best meet user needs.`,
     'icon': 'widgets'
   },
   'adtran2': {
     'title': 'System Software Team Co-op',
     'company': 'ADTRAN',
-    'location': 'Huntsville, AL',
+    'location': 'Huntsville, AL | Full-time',
     'time': 'Aug 2015 - Dec 2015',
-    'description': `Optimize current data structure algorithms in C++ and C using STL containers.
-        Construct more efficient data structure for OS timer handling and implement into the existing
-        kernel code. Write many tests for own code with gtest framework.`,
+    'description': `
+Optimize current data structure algorithms in C++ and C using STL containers.
+
+Construct more efficient data structure for OS timer handling and implement into the existing kernel code. Write many tests for own code with gtest framework.`,
     'icon': 'sd_card'
   },
   'adtran3': {
     'title': 'Systems Design and Verification Test Co-op',
     'company': 'ADTRAN',
-    'location': 'Huntsville, AL',
+    'location': 'Huntsville, AL | Full-time',
     'time': 'Jan 2015 - May 2015',
-    'description': `Work closely with a Triple Play Testbed. Upgrade the network, configure new services
-        and write python scripts and python app to speed up redundant interaction with testbed and other
-        telnet accessible lab equipment.`,
+    'description': `
+Work closely with a Triple Play Testbed.
+
+Upgrade the network, configure new services. 
+
+Write python scripts and python app to speed up redundant interaction with testbed and other telnet accessible lab equipment.`,
     'icon': 'build'
   }
 }
